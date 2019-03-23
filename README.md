f5b7794fce809aac0e43243fc160e84f4b17fbf6
Jeff White <karttoon@gmail.com>
2019-03-23 00:08:40 -0400

    Public release

--- README.md
+++ README.md
@@ -1,2 +1,157 @@
 # mffgear
-Marvel Future Fight script that helps in identifying which gear might be useful across your roster.
+MFFGear is a Marvel Future Fight (MFF) script that helps in identifying which gear might be useful across your roster.
+
+After a year+ into MFF I started to notice that I was storing a lot of custom gears using up valuable inventory space. I kept telling myself "I'll get back to these later and figure out who to put them on" but I never would and it became a huge PITA managing gear. I created this script to help solve that problem for myself but figured I'd make it available to anyone else if they find themselves in the same boat as me.
+
+Effectively, you pass it a comma delimited set of gear abbreviations and it'll tell you if a characters PVP/PVE optimal gear aligns, sorted by tier, rank, and whether they already have gear on. It has a handful of flags I've added to make usage slightly easier.
+
+```
+$ python mffgear.py -h
+usage: mffgear.py [-h] [-f <value>] [-m] [-v] [-u] [-d] [-i]
+
+Script for helping find Marvel Future Fight characters to equip gear on.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f <value>, --file <value>
+                        The file to use if not "mff.db".
+  -m, --merge           Download the latest character DB and merge it with
+                        file specified using -f flag.
+  -v, --verbose         Prompt for gear updates when listing.
+  -u, --update          Update your character DB with changes.
+  -d, --defs            Print gear abbreviations
+  -i, --init            Setup your characters for the first time.
+```
+
+You'll want to learn the gear abbreviations with `-d` to make usage quicker.
+
+```
+$ python mffgear.py -d
+
+[+] Gear Abbreviations
+
+{
+    "AD": "All Defense",
+    "CD": "Cold Damage",
+    "CRD": "Critical Damage",
+    "CRR": "Critical Rate",
+    "DEF": "Ignore Defense",
+    "DO": "Dodge",
+    "DP": "Damage Proc",
+    "FD": "Fire Damage",
+    "GB": "Guard Break Immunity",
+    "HP": "Max HP",
+    "ID": "Ignore Dodge",
+    "IP": "Invincible Proc",
+    "LD": "Lightning Damage",
+    "MD": "Mind Damage",
+    "PD": "Poison Damage",
+    "RR": "Recovery Rate"
+}
+
+[+] CTP Abbreviations
+
+{
+    "AUT": "CTP of Authority",
+    "DES": "CTP of Destruction",
+    "ENR": "CTP of Energy",
+    "PAT": "CTP of Patience",
+    "RAG": "CTF of Rage",
+    "REF": "CTP of Refinement",
+    "REG": "CTP of Regeneration",
+    "TRA": "CTP of Transcendence"
+}
+```
+
+To start it off, you can run the `-i` flag to initialize your roster and update it to reflect what you have (tier and gear per character). This will admittedly take a bit of time and you technically can skip it and just show all of the characters as if they were ungeared/default tiers. Additionally, you can use the `-m` flag to download the latest DB file and merge it with your current. While I can't promise I'll always keep it up-to-date, I do plan quite a bit daily and I'm happy to receive pull requests to flush out the existing DB.
+
+```
+$ python mffgear.py -m
+
+[!] Downloading latest DB file from https://raw.githubusercontent.com/karttoon/mffgear/master/mff_master.db
+[!] Found new gear definitions
+[!] Adding new character Juggernaut
+[!] Adding new character Sabertooth
+[!] Adding new character Warpath
+```
+
+I've built 1.0 to use the data from the character ranks according to [erceyazici](https://www.reddit.com/user/erceyazici)'s [Tier List v4.8.2](https://www.reddit.com/r/future_fight/comments/auw0b5/tier_list_v482/) and the PVP/PVE/CTP gears according to [fmv13](https://www.reddit.com/user/fmv13)'s [Character Building Power Book V.4.7](https://www.reddit.com/r/future_fight/comments/akfzgf/character_building_power_book_v47/). These two deserve a TON of credit for all the amazing work they put into these releases for the community.
+
+So what does it look like when you run it? Here is a sample output looking for a custom gear that has Guard Break Immunity (gb), Lightning Damage (ld), and a Invincible Proc (ip):
+
+```
+$ python mffgear.py -f kart_mff.db 'gb,ld,ip' -v
+
+[+] Candidate for Gear
+
+[01] [Tier 2 | Rank S | Gear PVP] Thor - GB, LD, IP
+
+[+] Enter the character number to update or Enter to skip: 1
+
+[+] Updating Thor
+	[-] Use format "CRR 25" to indicate Crit Rate 25% or "ENR" for CTP of Energy
+
+Slot 1: gb
+Slot 2: ld 31
+Slot 3: ip 3
+```
+
+Here's a slightly more generic search (intended for when I have an Ignore Defense gear with two other good stats that I want to temporarily use). In this case it's search for characters with either PVP or PVE slots matching Crit Damage and Invincible Proc.
+
+```
+$ python mffgear.py -f kart_mff.db -v 'crd,ip'
+
+[+] Candidate for Gear
+
+[01] [Tier 2 | Rank S | Gear PVP] Winter Soldier   - GB, CRD, IP
+[02] [Tier 2 | Rank S | Gear PVP] Proxima Midnight - GB, CRD, IP
+[03] [Tier 2 | Rank S | Gear PVP] Cull Obsidian    - HP, CRD, IP
+[04] [Tier 2 | Rank S | Gear PVP] Invisible Woman  - GB, CRD, IP
+[05] [Tier 2 | Rank S | Gear PVP] Corvus Glaive    - GB, CRD, IP
+[06] [Tier 2 | Rank S | Gear PVP] Doctor Doom      - GB, CRD, IP
+[07] [Tier 2 | Rank S | Gear PVP] Thanos           - GB, CRD, IP
+[08] [Tier 2 | Rank S | Gear PVP] Magneto          - GB, CRD, IP
+[09] [Tier 2 | Rank S | Gear PVP] Stryfe           - GB, CRD, IP
+[10] [Tier 2 | Rank A | Gear PVP] Adam Warlock     - GB, CRD, IP
+[11] [Tier 2 | Rank A | Gear PVP] Crystal          - GB, CRD, IP
+[12] [Tier 2 | Rank A | Gear PVP] Fantomex         - CRD, HP, IP
+[13] [Tier 2 | Rank A | Gear PVP] Domino           - CRD, DO, IP
+[14] [Tier 2 | Rank B | Gear ALL] Groot            - HP, CRD, IP
+[15] [Tier 2 | Rank B | Gear ALL] Falcon           - GB, CRD, IP
+[16] [Tier 1 | Rank S | Gear PVP] Nova             - GB, CRD, IP
+[17] [Tier 1 | Rank S | Gear PVP] Weapon Hex       - GB, CRD, IP
+[18] [Tier 1 | Rank A | Gear PVP] Magik            - GB, CRD, IP
+[19] [Tier 1 | Rank A | Gear ALL] Kid Kaiju        - CRD, GB, IP
+[20] [Tier 1 | Rank A | Gear PVP] Morgan Le Fay    - GB, CRD, IP
+[21] [Tier 1 | Rank A | Gear PVP] Hyperion         - GB, CRD, IP
+[22] [Tier 1 | Rank A | Gear PVP] Star-Lord        - CRD, DO, IP
+[23] [Tier 1 | Rank A | Gear PVP] Iron Hammer      - GB, CRD, IP
+[24] [Tier 1 | Rank A | Gear PVP] Killmonger       - HP, CRD, IP
+[25] [Tier 1 | Rank A | Gear PVP] Titania          - HP, CRD, IP
+[26] [Tier 1 | Rank B | Gear ALL] Spider-Gwen      - DO, CRD, IP
+[27] [Tier 1 | Rank B | Gear ALL] Goliath          - CRD, HP, IP
+
+[+] Already Geared
+
+[28] [Tier 2 | Rank S | Current ] Doctor Strange   - Critical Rate: 22, Guard Break Immunity: 0, Damage Proc: 100
+[29] [Tier 2 | Rank S | Current ] Ebony Maw        - Guard Break Immunity: 0, Invincible Proc: 3, Critical Rate: 26
+[30] [Tier 1 | Rank B | Current ] Warwolf          - Critical Rate: 20, Invincible Proc: 2, Critical Rate: 13
+[31] [Tier 2 | Rank S | Current ] Ant-Man          - Critical Damage: 22, Invincible Proc: 2, Dodge: 14
+
+[+] Enter the character number to update or Enter to skip: 4
+
+[+] Updating Invisible Woman
+	[-] Use format "CRR 25" to indicate Crit Rate 25% or "ENR" for CTP of Energy
+
+Slot 1: crd 25
+Slot 2: def 12
+Slot 3: ip 4
+```
+
+When I run the same search again, Invisible Woman will now be listed under "Already Geared":
+
+```
+[29] [Tier 2 | Rank S | Current ] Invisible Woman  - Critical Damage: 25, Invincible Proc: 4, Ignore Defense: 12
+```
+
+That's about it. I've included my personal DB file (kart_mff.db) if you want to use it as a test.
---
+++ mffgear.py
@@ -0,0 +1,308 @@
+#!/usr/bin/env python
+import json, sys, argparse, requests, os
+
+__author__  = "Jeff White [karttoon] @noottrak"
+__email__   = "karttoon@gmail.com"
+__version__ = "1.0.0"
+__date__    = "22MAR2019"
+
+requests.packages.urllib3.disable_warnings()
+
+def getMaster():
+
+    url = "https://raw.githubusercontent.com/karttoon/mffgear/master/mff_master.db"
+
+    print "\n[!] Downloading latest DB file from %s" % (url)
+
+    data = json.loads(requests.get(url, verify=False).content)
+
+    return data
+
+def findGear(outResult, data, userResponse):
+
+    for mffChar in data["chars"]:
+
+        outResult[mffChar] = {}
+        charGear = {}
+
+        for gearSet in data["chars"][mffChar]["gear"]:
+            charGear[gearSet] = list(data["chars"][mffChar]["gear"][gearSet])
+
+        for gearSet in charGear:
+
+            tempGear = list(charGear[gearSet])
+            lenCheck = len(tempGear)
+
+            for itemName in userResponse:
+                if itemName in tempGear:
+                    tempGear.remove(itemName)
+
+            if len(userResponse) == 2:
+                if len(tempGear) == 1 and lenCheck != 1:
+                    outResult[mffChar][gearSet] = charGear[gearSet]
+            else:
+                if len(tempGear) == 0 and lenCheck != 0:
+                    outResult[mffChar][gearSet] = charGear[gearSet]
+
+    for mffChar, charGear in outResult.items():
+        if charGear == {}:
+            del outResult[mffChar]
+
+    return outResult
+
+def buildPrint(outResult, data):
+
+    alreadyGear = []
+
+    printMsg = []
+
+    # Determining print length for names
+    maxLen = 0
+    for mffChar in outResult:
+        if len(mffChar) >= maxLen:
+            maxLen = len(mffChar)
+
+    # Loops through Tier 3 -> 1
+    for i in range(3,0,-1):
+
+        charTier = {"S": [], "A": [], "B": [], "C": [], "D": [], "?": []}
+
+        for mffChar in outResult:
+
+            if data["chars"][mffChar]["current"] != {}:
+                alreadyGear.append(mffChar)
+
+            if data["chars"][mffChar]["tier"] == i and data["chars"][mffChar]["current"] == {}:
+                charTier[data["chars"][mffChar]["rank"]].append(mffChar)
+
+        for tierLvl in ["S", "A", "B", "C", "D", "?"]:
+            for mffChar in charTier[tierLvl]:
+                gearType = outResult[mffChar].keys()[0]
+                printMsg.append("![Tier %s | Rank %s | Gear %s] %-*s - %s" % (data["chars"][mffChar]["tier"],
+                                              data["chars"][mffChar]["rank"],
+                                              gearType.upper(),
+                                              maxLen,
+                                              mffChar,
+                                              ", ".join(outResult[mffChar][gearType])))
+
+    # Build print for candidates who already have gear defined
+    if len(alreadyGear) >= 1:
+        for mffChar in set(alreadyGear):
+
+            printString = "?[Tier %s | Rank %s | Current ] %-*s - " % (data["chars"][mffChar]["tier"],
+                                                                  data["chars"][mffChar]["rank"],
+                                                                  maxLen,
+                                                                  mffChar)
+
+            for slotValue in data["chars"][mffChar]["current"]:
+                for entry in data["chars"][mffChar]["current"][slotValue]:
+
+                    if entry in data["defs"]["gear"]:
+                        atrName = data["defs"]["gear"][entry]
+                    else:
+                        atrName = data["defs"]["ctp"][entry]
+                    atrValue = data["chars"][mffChar]["current"][slotValue][entry]
+
+                printString += "%s: %s, " % (atrName, atrValue)
+
+            printMsg.append(printString[:-2])
+
+    return printMsg
+
+def outPrint(printMsg, args, data):
+
+    alreadyGear = 0
+    candidateGear = 0
+
+    for count, row in enumerate(printMsg):
+
+        if row.startswith("!"):
+            if candidateGear == 0:
+                print "\n[+] Candidate for Gear\n"
+                candidateGear = 1
+            print "[%.2d] %s " % (count + 1, row[1:])
+
+        if row.startswith("?"):
+            if alreadyGear == 0:
+                print "\n[+] Already Geared\n"
+                alreadyGear = 1
+            print "[%.2d] %s " % (count + 1, row[1:])
+
+    if args.verbose:
+
+        userInput = raw_input("\n[+] Enter the character number to update or Enter to skip: ")
+
+        if userInput.isdigit():
+
+            charPick = int(userInput) - 1
+
+            while charPick > len(printMsg):
+                print "\n[!] Not a valid pick"
+
+            mffChar = printMsg[charPick].split("]")[1].split(" - ")[0].strip()
+            data = updateChar(mffChar, data)
+
+        else:
+            print "\n[!] Skipping update process"
+            pass
+
+    else:
+        print
+
+    return data
+
+def updateChar(mffChar, data):
+
+    print "\n[+] Updating %s\n\t[-] Use format \"CRR 25\" to indicate Crit Rate 25%% or \"ENR\" for CTP of Energy\n" % (mffChar)
+
+    for slotValue in range(1,4):
+
+        userInput = raw_input("Slot %s: " % (slotValue))
+        gearType = userInput.split(" ")[0].upper()
+
+        if gearType not in data["defs"]["gear"] and gearType not in data["defs"]["ctp"]:
+            print "\n[!] Not a valid gear abbreviation - use `-d` flag to see defined entries"
+            break
+
+        if len(userInput.split(" ")) > 1 and userInput.split(" ")[1].isdigit():
+            gearValue = int(userInput.split(" ")[1])
+        else:
+            gearValue = 0
+
+        if gearType in data["defs"]["ctp"]:
+            data["chars"][mffChar]["current"]["slot1"] = {gearType:0}
+            break
+        else:
+            data["chars"][mffChar]["current"]["slot%s" % (slotValue)] = {gearType:gearValue}
+
+    print
+    return data
+
+def mergeDB(data):
+
+    mainDB = getMaster()
+
+    newUpdate = 0
+
+    # Update definitions for attributes/CTPs
+    if mainDB["defs"] != data["defs"]:
+        print "[!] Found new gear definitions"
+        data["defs"] = mainDB["defs"]
+        newUpdate = 1
+
+    # Check for new chars and new gear/ranks
+    for mffChar in mainDB["chars"]:
+
+        # Add new chars
+        if mffChar not in data["chars"]:
+            print "[!] Adding new character %s" % (mffChar)
+            data["chars"][mffChar] = mainDB["chars"][mffChar]
+            newUpdate = 1
+
+        # Check for rank change
+        if mainDB["chars"][mffChar]["rank"] != data["chars"][mffChar]["rank"]:
+            print "[!] Changing rank for %s from %s to %s" % (mffChar,
+                                                              data["chars"][mffChar]["rank"],
+                                                              mainDB["chars"][mffChar]["rank"])
+            data["chars"][mffChar]["rank"] = mainDB["chars"][mffChar]["rank"]
+            newUpdate = 1
+
+        # Check for gear changes
+        for gearType in mainDB["chars"][mffChar]["gear"]:
+            if mainDB["chars"][mffChar]["gear"][gearType] != data["chars"][mffChar]["gear"][gearType]:
+                print "[!] Updating %s's %s gear to:\n%s" % (mffChar, gearType.upper(), json.dumps(mainDB["chars"][mffChar]["gear"][gearType], indent=4, sort_keys=True))
+                data["chars"][mffChar]["gear"][gearType] = mainDB["chars"][mffChar]["gear"][gearType]
+                newUpdate = 1
+
+    if newUpdate == 0:
+        print "[!] No new updates"
+    else:
+        print
+
+    return data
+
+def charSetup(args, data):
+
+    for mffChar in data["chars"]:
+
+        userInput = raw_input("\n[!] Edit %s? [Y/N or Enter] - " % (mffChar)).upper()
+
+        if userInput == "Y":
+
+            userInput = input("\t[=] Current Tier? - ")
+            data["chars"][mffChar]["tier"] = userInput
+
+            data = updateChar(mffChar, data)
+
+        saveFile(args, data)
+
+    return data
+
+def saveFile(args, data):
+
+    writeOut = open(args.file, "w")
+    writeOut.write(json.dumps(data, indent=4, sort_keys=True))
+    writeOut.close()
+
+    return
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Script for helping find Marvel Future Fight characters to equip gear on.")
+    parser.add_argument("-f", "--file", help="The file to use if not \"mff.db\".", metavar="<value>", default="mff.db")
+    parser.add_argument("-m", "--merge", help="Download the latest character DB and merge it with file specified using -f flag.", action="store_true")
+    parser.add_argument("-v", "--verbose", help="Prompt for gear updates when listing.", action="store_true")
+    parser.add_argument("-u", "--update", help="Update your character DB with changes.", action="store_true")
+    parser.add_argument("-d", "--defs", help="Print gear abbreviations", action="store_true")
+    parser.add_argument("-i", "--init", help="Setup your characters for the first time.", action="store_true")
+    args, unkargs = parser.parse_known_args()
+
+    if os.path.isfile(args.file):
+        data = json.load(open(args.file))
+    else:
+        data = getMaster()
+        saveFile(args, data)
+
+    try:
+
+        if len(unkargs) >= 1:
+            for value in unkargs[0].upper().split(","):
+                if value not in data["defs"]["gear"] and value not in data["defs"]["ctp"]:
+                    print "[!] Unknown gear type %s" % (value)
+                    sys.exit(1)
+            userResponse = unkargs[0].upper().split(",")
+        else:
+            userResponse = []
+
+        if (args.defs or args.init):
+
+            print "\n[+] Gear Abbreviations\n"
+            print json.dumps(data["defs"]["gear"], indent=4, sort_keys=True)
+
+            print "\n[+] CTP Abbreviations\n"
+            print json.dumps(data["defs"]["ctp"], indent=4, sort_keys=True)
+
+        if args.init:
+            charSetup(args, data)
+
+        elif args.merge:
+            data = mergeDB(data)
+            saveFile(args, data)
+
+        else:
+            outResult = {}
+            outResult = findGear(outResult, data, userResponse)
+            printMsg = buildPrint(outResult, data)
+            if printMsg != []:
+                data = outPrint(printMsg, args, data)
+                saveFile(args, data)
+
+    except Exception as error:
+
+        print "\n[!] Ran into an error. Saving DB.\n%s" % (error)
+        saveFile(args, data)
+
+    return
+
+if __name__ == '__main__':
+    main()
